f245e73ae8c283e1fbc37fa58d36315e4f47b9d6
yogeshprakhar <ygeshprakharpal200027@gmail.com>
2024-08-18 23:32:31 +0530

    udpate cookie

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -42,6 +42,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie("auth_token", token, {
       httpOnly: true,
+      secure:true,
     });
     res.status(200).json({ userId: user._id });
   } catch (error) {
